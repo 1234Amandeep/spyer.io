38eb0e6ebeda248d6d5dbc58880fc670403bff04
Amandeep Singh <a.d.scr71234@gmail.com>
2024-01-05 16:18:53 +0530

    changing map tile layer

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -27,8 +27,8 @@ export default function Map() {
           scrollWheelZoom={true}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           {playerProfile && (
             <Marker position={playerProfile.latLng}>
